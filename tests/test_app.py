cbf17653628169b80e50ee84e75827d7990ed846
Paulo Mendonça <paulo.mendonca@visie.com.br>
2024-10-12 15:17:51 -0300

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from splice.app import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get('/')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'message': 'Olá Mundo!'}
