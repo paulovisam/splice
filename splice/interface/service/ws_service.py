716049f0e647f1da27fa18340bc4fb6d5e898785
Paulo Mendonça <paulo.mendonca@visie.com.br>
2024-12-31 15:19:42 -0300

    feat: add ws router

--- splice/interface/service/ws_service.py
+++ splice/interface/service/ws_service.py
@@ -1,38 +1,40 @@
 from fastapi import WebSocket, WebSocketDisconnect
-
+from splice.infra.repositories.redis_repository import RedisRepository
+global clients
+clients = {}
 
 class WSService:
     def __init__(self):
-        PORTS = [8080]
-        self.clients = {port: {} for port in PORTS}
-
+        self.clients = clients
+        self.redis = RedisRepository()
     async def handle_client(
-        self, websocket: WebSocket, port: int, chat_id: str
+        self, websocket: WebSocket, chat_id: str
     ):
+        
         await websocket.accept()
-        if chat_id not in self.clients[port]:
-            self.clients[port][chat_id] = set()
-        self.clients[port][chat_id].add(websocket)
+        if chat_id not in self.clients:
+            self.clients[chat_id] = set()
+        self.clients[chat_id].add(websocket)
+        # self.redis.add_client_ws(chat_id=chat_id, websocket=websocket)
 
-        print(
-            f'Conexão estabelecida com {websocket.client}, chat_id: {chat_id}'
-        )
+        print(f'Conexão estabelecida com {websocket.client}, chat_id: {chat_id}')
+        print(self.clients[chat_id])
+        print(len(self.clients[chat_id]))
 
         try:
             while True:
-                message = (
-                    await websocket.receive_text()
-                )  # Recebe uma mensagem de texto
+                message = await websocket.receive_text()
                 print(
-                    f'Mensagem recebida de {websocket.client} na porta {port}, chat_id: {chat_id}: {message}'
+                    f'Mensagem recebida de {websocket.client}, chat_id: {chat_id}: {message}'
                 )
-
-                # Enviar para todos os clientes conectados, exceto o remetente
-                for client in self.clients[port][chat_id]:
+                
+                # for client in self.clients[chat_id]:
+                for client in self.redis.get_clients_ws(chat_id=chat_id):
                     if client != websocket:
                         await client.send_text(message)
         except WebSocketDisconnect:
-            self.clients[port][chat_id].remove(websocket)
-            print(
-                f'Cliente {websocket.client} desconectado na porta {port}, chat_id: {chat_id}'
-            )
+            # self.clients[chat_id].remove(websocket)
+            self.redis.remove_client_ws(chat_id=chat_id)
+            if not self.clients[chat_id]:
+                del self.clients[chat_id]
+            print(f'Cliente {websocket.client} desconectado, chat_id: {chat_id}')
