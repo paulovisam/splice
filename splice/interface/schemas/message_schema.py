22694d0cbcb4bd2f0213c2a72aba93ae2b274d48
Paulo Mendonça <paulo.mendonca@visie.com.br>
2025-01-05 11:24:30 -0300

    feat: add new fiels in MessageSchema

--- splice/interface/schemas/message_schema.py
+++ splice/interface/schemas/message_schema.py
@@ -1,17 +1,18 @@
 from pydantic import BaseModel
 from typing import Optional
-
+from datetime import datetime
 
 class MessageCreateSchema(BaseModel):
     content: str
     sender: str
     receiver: str
-
+    created_at: datetime = datetime.now()
 
 class MessageUpdateSchema(BaseModel):
     content: str = None
     sender: str = None
     receiver: str = None
+    updated_at: datetime = datetime.now()
 
 
 class MessageResponseSchema(BaseModel):
@@ -19,3 +20,6 @@ class MessageResponseSchema(BaseModel):
     content: Optional[str] = None
     sender: Optional[str] = None
     receiver: Optional[str] = None
+    created_at: datetime
+    updated_at: Optional[datetime] = None
+
