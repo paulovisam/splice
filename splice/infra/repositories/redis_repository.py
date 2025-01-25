664a78c90dbf95e5a5ad5cfb3b96b5a245ef2bae
Paulo Mendonça <paulo.mendonca@visie.com.br>
2025-01-25 10:49:48 -0300

    improv: args ins function

--- splice/infra/repositories/redis_repository.py
+++ splice/infra/repositories/redis_repository.py
@@ -5,8 +5,8 @@ class RedisRepository:
     def __init__(self):
         self.redis = redis_session
 
-    def add_client_ws(self, chat_id, websocket):
-        self.redis.sadd(chat_id, websocket)
+    def add_client_ws(self, chat_id, *args):
+        self.redis.sadd(chat_id, *args)
 
     def remove_client_ws(self, chat_id, websocket):
         self.redis.srem(chat_id, websocket)
